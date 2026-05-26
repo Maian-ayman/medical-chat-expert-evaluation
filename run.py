df0fdc67656dae71bc254304d463beb8d3c48da9
Maian-ayman <maian_ayman@yahoo.com>
2026-05-26 04:43:30 +0300

    Update Uvicorn app import path

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
 
     uvicorn.run(
-        "app:app",
+        "app.main:app",
         host="0.0.0.0",
         port=port
     )
